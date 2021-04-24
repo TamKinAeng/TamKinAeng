6e1644f09eb9eadfe78bb1b6cb5ff2602dc725d1
NewTHESimper <newjanpi1540@gmail.com>
2021-04-24 15:55:50 +0700

    [FIX] Category Size

--- tam_kin_aeng_mobile_app/lib/screen/home/component/categories.dart
+++ tam_kin_aeng_mobile_app/lib/screen/home/component/categories.dart
@@ -45,92 +45,96 @@ class CategoriesData extends StatelessWidget {
           return Text('Loading...');
         }
 
-        return ListView.builder(
-          shrinkWrap: true,
-          itemCount: snapshot.data.docs.length,
-          itemBuilder: (context, index) {
-            return snapshot.data.docs[index]['cuisine']
-                        .toString()
-                        .toLowerCase() ==
-                    cuisine.toLowerCase()
-                ? InkWell(
-                    // use inkWell to make our recipe on each categories clickable
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => RecipeScreen(
-                                    recipeIndex: snapshot.data.docs[index],
-                                  )));
-                    },
-                    child: Container(
-                      height: MediaQuery.of(context).size.height * 0.13,
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.symmetric(
-                                vertical: defaultSize * 0.5),
-                            child: Row(
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.fromLTRB(
-                                      0, 0, defaultSize * 3, 0),
-                                  child: Image.network(
-                                    snapshot.data.docs[index]['imgUrl'],
-                                    height: defaultSize * 7.5, //75
-                                    width: defaultSize * 10, //100
-                                    fit: BoxFit.fill,
-                                    // scale: 0.8,
+        return Container(
+          height: defaultSize * 3, //30
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: snapshot.data.docs.length,
+            itemBuilder: (context, index) {
+              return snapshot.data.docs[index]['cuisine']
+                          .toString()
+                          .toLowerCase() ==
+                      cuisine.toLowerCase()
+                  ? InkWell(
+                      // use inkWell to make our recipe on each categories clickable
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => RecipeScreen(
+                                      recipeIndex: snapshot.data.docs[index],
+                                    )));
+                      },
+                      child: Container(
+                        height: MediaQuery.of(context).size.height * 0.15, //15%
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                vertical: defaultSize * 0.5, //5
+                              ),
+                              child: Row(
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(
+                                        0, 0, defaultSize * 3, 0),
+                                    child: Image.network(
+                                      snapshot.data.docs[index]['imgUrl'],
+                                      height: defaultSize * 7.5, //75
+                                      width: defaultSize * 10, //100
+                                      fit: BoxFit.fill,
+                                      // scale: 0.8,
+                                    ),
                                   ),
-                                ),
-                                Container(
-                                  height: defaultSize * 7.5,
-                                  width: defaultSize * 23,
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        snapshot.data.docs[index]['name'],
-                                        style: TextStyle(
-                                          color: Colors.black,
-                                          fontWeight: FontWeight.bold,
+                                  Container(
+                                    height: defaultSize * 7.5,
+                                    width: defaultSize * 23,
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          snapshot.data.docs[index]['name'],
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.bold,
+                                          ),
                                         ),
-                                      ),
-                                      Text(
-                                        snapshot.data.docs[index]
-                                            ['description'],
-                                        style: TextStyle(
-                                          color: Colors.grey,
+                                        Text(
+                                          snapshot.data.docs[index]
+                                              ['description'],
+                                          style: TextStyle(
+                                            color: Colors.grey,
+                                          ),
+                                          maxLines: 3,
+                                          overflow: TextOverflow.ellipsis,
                                         ),
-                                        maxLines: 3,
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                    ],
+                                      ],
+                                    ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(
-                              vertical: defaultSize * 1,
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                vertical: defaultSize * 1,
+                              ),
+                              child: Container(
+                                color: Colors.black,
+                                height: defaultSize * 0.1,
+                                width: defaultSize * 50,
+                              ),
                             ),
-                            child: Container(
-                              color: Colors.black,
-                              height: defaultSize * 0.1,
-                              width: defaultSize * 50,
-                            ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                  )
-                : Container();
-          },
-          // children: snapshot.data.docs.map((DocumentSnapshot documentSnapshot) {
+                    )
+                  : Container();
+            },
+            // children: snapshot.data.docs.map((DocumentSnapshot documentSnapshot) {
 
-          // }).toList(),
+            // }).toList(),
+          ),
         );
       },
     );
