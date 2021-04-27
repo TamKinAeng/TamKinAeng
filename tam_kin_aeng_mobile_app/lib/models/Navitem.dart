6de5a91ca94ff151d94b0babcb77794432d452c6
Xta-FamEz <thanyapoj.f@mail.kmutt.ac.th>
2021-04-27 17:01:11 +0700

    [UPDATE] Navbar
    + Navbar App Recipe

--- tam_kin_aeng_mobile_app/lib/models/Navitem.dart
+++ tam_kin_aeng_mobile_app/lib/models/Navitem.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tam_kin_aeng_mobile_app/screen/home/home_screen.dart';
 import 'package:tam_kin_aeng_mobile_app/screen/profile/profile_screen.dart';
+import 'package:tam_kin_aeng_mobile_app/screen/addRecipe/addRecipe.dart';
 
 class NavItem {
   final int id;
@@ -39,6 +40,7 @@ class NavItems extends ChangeNotifier {
     NavItem(
       id: 2,
       icon: "assets/icons/list.svg",
+      destination: AddRecipe(),
     ),
     NavItem(
       id: 3,
