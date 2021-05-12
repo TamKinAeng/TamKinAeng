45ae9fb55c82f81fbfb735704f9500d6cb8693e6
Thanatat Wongabut <thanatat.wong@mail.kmutt.ac.th>
2021-05-13 03:11:19 +0700

    [UPDATE] Card Recipe Page

--- tam_kin_aeng_mobile_app/lib/screen/addRecipe/component/body.dart
+++ tam_kin_aeng_mobile_app/lib/screen/addRecipe/component/body.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -8,17 +6,13 @@ import 'package:google_fonts/google_fonts.dart';
 
 import 'EditPage.dart';
 
-
-
 class AddRecipeBody extends StatefulWidget {
-
   //static String uid;
   @override
   _AddRecipeBodyState createState() => _AddRecipeBodyState();
 }
 
 class _AddRecipeBodyState extends State<AddRecipeBody> {
-
   String uid;
   Future<String> getUID() async {
     FirebaseAuth auth = FirebaseAuth.instance;
@@ -38,50 +32,64 @@ class _AddRecipeBodyState extends State<AddRecipeBody> {
   }
 
   @override
-  Widget build(BuildContext context)  {
-    final ref = FirebaseFirestore.instance.collection('Users').doc(uid).collection('AddRecipe');
+  Widget build(BuildContext context) {
+    final ref = FirebaseFirestore.instance
+        .collection('Users')
+        .doc(uid)
+        .collection('AddRecipe');
     return Scaffold(
       body: StreamBuilder(
         stream: ref.snapshots(),
-        builder: (context,AsyncSnapshot<QuerySnapshot> snapshot) {
-            return Container(
-                child: GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2),
-                    itemCount: snapshot.hasData ? snapshot.data.docs.length : 0,
-                    itemBuilder: (_, index) {
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.push(context, MaterialPageRoute(
-                              builder: (context) =>
-                                  EditPage(
-                                    docToEdit: snapshot.data.docs[index],)));
-                        },
-                        child: Container(
-                          margin: EdgeInsets.all(10),
-                          height: 150,
-                          decoration: BoxDecoration(
-                            color: Colors.orange[200],
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              children: [
-                                Text(snapshot.data.docs[index].data()['name'],
-                                    style: TextStyle(fontSize: 25)),
-                                Text(snapshot.data.docs[index]
-                                    .data()['description'],
-                                  style: TextStyle(fontSize: 15),
-                                  overflow: TextOverflow.ellipsis, maxLines: 5,),
-                              ],
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          return Container(
+            child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemCount: snapshot.hasData ? snapshot.data.docs.length : 0,
+                itemBuilder: (_, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => EditPage(
+                                    docToEdit: snapshot.data.docs[index],
+                                  )));
+                    },
+                    child: Container(
+                      margin: EdgeInsets.all(10),
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFFF6D00),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text(snapshot.data.docs[index].data()['name'],
+                                style: GoogleFonts.roboto(
+                                    textStyle: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold))),
+                            Text(
+                              snapshot.data.docs[index].data()['description'],
+                              style: GoogleFonts.roboto(
+                                  textStyle: TextStyle(
+                                fontSize: 15,
+                                color: Colors.white,
+                              )),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 5,
                             ),
-                          ),
+                          ],
                         ),
-                      );
-                    }),
-              );
-
+                      ),
+                    ),
+                  );
+                }),
+          );
         },
       ),
     );
